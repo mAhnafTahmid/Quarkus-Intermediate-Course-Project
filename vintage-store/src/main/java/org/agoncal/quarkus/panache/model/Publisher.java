69eb6946e9c12b2c4660ef96c611fea1df177935
Ahnaf Tahmid <ahnaf.tahmid1@g.bracu.ac.bd>
2026-01-24 13:19:53 +0600

    Used annotation to change table properties for customer & publisher

--- vintage-store/src/main/java/org/agoncal/quarkus/panache/model/Publisher.java
+++ vintage-store/src/main/java/org/agoncal/quarkus/panache/model/Publisher.java
@@ -3,11 +3,18 @@ package org.agoncal.quarkus.panache.model;
 import java.time.Instant;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "t_publishers")
 public class Publisher extends PanacheEntity {
+
+    @Column(length = 50, nullable = false)
     public String name;
+
+    @Column(name = "created_date", nullable = false)
     public Instant createdDate = Instant.now();
 
     public Publisher() {

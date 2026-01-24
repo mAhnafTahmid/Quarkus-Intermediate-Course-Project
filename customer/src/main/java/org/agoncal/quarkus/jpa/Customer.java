c3b854b7233863ff69245f4850c8fdccc9468c80
Ahnaf Tahmid <ahnaf.tahmid1@g.bracu.ac.bd>
2026-01-24 13:20:45 +0600

    Used annotation to change table properties for customer & publisher

--- customer/src/main/java/org/agoncal/quarkus/jpa/Customer.java
+++ customer/src/main/java/org/agoncal/quarkus/jpa/Customer.java
@@ -2,23 +2,30 @@ package org.agoncal.quarkus.jpa;
 
 import java.time.Instant;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "t_customers")
 public class Customer {
 
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(name = "first_name", length = 50, nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", length = 50, nullable = false)
     private String lastName;
+
+    @Column(name = "e_mail", nullable = false)
     private String email;
+
+    @Column(name = "created_date", nullable = false)
     private Instant createdDate = Instant.now();
 
     public Customer() {

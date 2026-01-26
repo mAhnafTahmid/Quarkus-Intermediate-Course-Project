6f526868e3960b99898907746df145e74f13e79a
Ahnaf Tahmid <ahnaf.tahmid1@g.bracu.ac.bd>
2026-01-26 11:46:12 +0600

    Added new sub-classes for Publisher and mapped their inheritance to SQL relationships

---
+++ vintage-store/src/main/java/org/agoncal/quarkus/panache/model/Book.java
@@ -0,0 +1,25 @@
+package org.agoncal.quarkus.panache.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+@Entity
+public class Book extends Item {
+
+    @Column(length = 15)
+    public String isbn;
+
+    @Column(name = "nb_of_pages")
+    public int nbOfPages;
+
+    @Column(name = "publication_date")
+    public LocalDate publicationDate;
+
+    @Enumerated(EnumType.STRING)
+    public Language language;
+
+}
---
+++ vintage-store/src/main/java/org/agoncal/quarkus/panache/model/Item.java
@@ -0,0 +1,30 @@
+package org.agoncal.quarkus.panache.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "t_items")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Item extends PanacheEntity {
+
+    @Column(length = 100, nullable = false)
+    public String name;
+
+    @Column(length = 3000)
+    public String description;
+
+    @Column(nullable = false)
+    public BigDecimal price;
+
+    @Column(name = "created_date", nullable = false)
+    public Instant createdDate = Instant.now();
+
+}

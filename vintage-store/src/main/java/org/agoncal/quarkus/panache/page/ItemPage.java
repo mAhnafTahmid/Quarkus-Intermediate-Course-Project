18d0fa75f47189486c4ae0b6a1002b70eee51cb2
Ahnaf Tahmid <ahnaf.tahmid1@g.bracu.ac.bd>
2026-02-03 13:14:59 +0600

    added pagination

--- vintage-store/src/main/java/org/agoncal/quarkus/panache/page/ItemPage.java
+++ vintage-store/src/main/java/org/agoncal/quarkus/panache/page/ItemPage.java
@@ -3,39 +3,19 @@ package org.agoncal.quarkus.panache.page;
 import java.util.List;
 
 import org.agoncal.quarkus.panache.model.Book;
+import org.agoncal.quarkus.panache.model.CD;
 import org.agoncal.quarkus.panache.model.Item;
 
+import io.quarkus.panache.common.Sort;
 import io.quarkus.qute.CheckedTemplate;
 import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
-
-// @Path("page/items")
-// @Produces(MediaType.TEXT_HTML)
-// public class ItemPage {
-
-//     @CheckedTemplate
-//     public static class Templates {
-//         public static native TemplateInstance book(Book book);
-
-//         public static native TemplateInstance books(List<Book> books);
-//     }
-
-//     @GET
-//     @Path("/books/{id}")
-//     public TemplateInstance showBookById(@PathParam("id") Long id) {
-//         return Templates.book(Book.findById(id));
-//     }
-
-//     @GET
-//     @Path("/books")
-//     public TemplateInstance showAllBooks() {
-//         return Templates.books(Book.listAll());
-//     }
-// }
 
 @Path("page/items")
 @Produces(MediaType.TEXT_HTML)
@@ -46,6 +26,10 @@ public class ItemPage {
         public static native TemplateInstance book(Book book);
 
         public static native TemplateInstance books(List<Book> books);
+
+        public static native TemplateInstance cd(CD cd);
+
+        public static native TemplateInstance cds(List<CD> cds);
     }
 
     @GET
@@ -59,9 +43,39 @@ public class ItemPage {
 
     @GET
     @Path("/books")
-    public TemplateInstance showAllBooks() {
-        // Query all items where dtype = 'Book' and map to Book
-        List<Book> books = Book.listAll();
-        return Templates.books(books);
+    public TemplateInstance showAllBooks(@QueryParam("query") String query,
+            @QueryParam("sort") @DefaultValue("id") String sort, @QueryParam("page") @DefaultValue("0") int pageIndex,
+            @QueryParam("size") @DefaultValue("1000") int pageSize) {
+        return Templates.books(Book.find(query, Sort.by(sort)).page(pageIndex, pageSize).list())
+                .data("query", query)
+                .data("sort", sort)
+                .data("pageIndex", pageIndex)
+                .data("pageSize", pageSize);
+    }
+
+    // @GET
+    // @Path("/books")
+    // public TemplateInstance showAllBooks() {
+    // // Query all items where dtype = 'Book' and map to Book
+    // List<Book> books = Book.listAll();
+    // return Templates.books(books);
+    // }
+
+    @GET
+    @Path("/cds/{id}")
+    public TemplateInstance showCDById(@PathParam("id") Long id) {
+        return Templates.cd(CD.findById(id));
+    }
+
+    @GET
+    @Path("/cds")
+    public TemplateInstance showAllCDs(@QueryParam("query") String query,
+            @QueryParam("sort") @DefaultValue("id") String sort, @QueryParam("page") @DefaultValue("0") int pageIndex,
+            @QueryParam("size") @DefaultValue("1000") int pageSize) {
+        return Templates.cds(CD.find(query, Sort.by(sort)).page(pageIndex, pageSize).list())
+                .data("query", query)
+                .data("sort", sort)
+                .data("pageIndex", pageIndex)
+                .data("pageSize", pageSize);
     }
 }
